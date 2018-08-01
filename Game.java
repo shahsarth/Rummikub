2276e96d743f40167e8a16e6facd110b84ad079e
Sarth Shah <shahsarth@gmail.com>
2018-07-31 19:02:24 -0700

    Game.java setup

---
+++ Game.java
@@ -0,0 +1,13 @@
+/** Invariants
+
+ 1. Pieces include numbers 1 - 13,
+ 2. With 4 colors Red, Black, Orange, Blue
+ 3. 2 pieces of each letter color comboination
+ 4. There are two jokers
+
+ */
+
+public class Game{
+    Game(){
+
+    }
